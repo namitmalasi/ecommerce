46a4341e160984f752acedef2fdd3c0f34ef3ed5
Namit Malasi <namitmalasi@gmail.com>
2023-10-18 12:33:27 +0530

    setup for redux

--- client/src/scenes/global/Navbar.js
+++ client/src/scenes/global/Navbar.js
@@ -8,11 +8,12 @@ import {
 } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 import { shades } from "../../theme";
+import { setIsCartOpen } from "../../state";
 
 const Navbar = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
+  const cart = useSelector((state) => state.cart.cart);
   return (
     <Box
       display="flex"
@@ -53,9 +54,29 @@ const Navbar = () => {
           <IconButton sx={{ color: "black" }}>
             <PersonOutline />
           </IconButton>
-          <IconButton sx={{ color: "black" }}>
-            <ShoppingBagOutlined />
-          </IconButton>
+
+          <Badge
+            badgeContent={cart.length}
+            color="secondary"
+            invisible={cart.length === 0}
+            sx={{
+              "& .MuiBadge-badge": {
+                right: 5,
+                top: 5,
+                padding: "0 4px",
+                height: "14px",
+                minWidth: "13px",
+              },
+            }}
+          >
+            <IconButton
+              onCLick={() => dispatch(setIsCartOpen({}))}
+              sx={{ color: "black" }}
+            >
+              <ShoppingBagOutlined />
+            </IconButton>
+          </Badge>
+
           <IconButton sx={{ color: "black" }}>
             <MenuOutlined />
           </IconButton>
