fd2007a8e70ffd670443509e9ce7b64a55f7c0f4
Namit Malasi <namitmalasi@gmail.com>
2023-10-24 13:13:34 +0530

    added carousel on the homepage

--- client/src/scenes/home/Home.js
+++ client/src/scenes/home/Home.js
@@ -1,7 +1,11 @@
-import React from "react";
+import MainCarousel from "./MainCarousel";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <MainCarousel />
+    </div>
+  );
 };
 
 export default Home;
